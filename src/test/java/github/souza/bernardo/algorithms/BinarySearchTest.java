c6e9f4017e15f6ffc05092fd70fe6c9afaa5714a
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2024-04-28 20:10:47 -0300

    binary search tests

--- src/test/java/github/souza/bernardo/algorithms/BinarySearchTest.java
+++ src/test/java/github/souza/bernardo/algorithms/BinarySearchTest.java
@@ -1,41 +1,31 @@
 package github.souza.bernardo.algorithms;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.stream.Stream;
+
+import static java.util.stream.Stream.of;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class BinarySearchTest {
 
-    @Test
-    public void find() {
-        BinarySearch binarySearch = new BinarySearch();
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int target = 3;
-        int result = binarySearch.search(array, target);
-        int resultRecursive = binarySearch.searchRecursive(array, target, 0 , 10);
-        assertEquals(2, result);
-        assertEquals(2, resultRecursive);
+    BinarySearch binarySearch = new BinarySearch();
+
+    @ParameterizedTest
+    @MethodSource("scenarios")
+    public void find(int target, int positionExpected) {
+        var array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        var result = binarySearch.search(array, target);
+        var resultRecursive = binarySearch.searchRecursive(array, target, 0, array.length);
+        assertEquals(positionExpected, result);
+        assertEquals(positionExpected, resultRecursive);
     }
 
-    @Test
-    public void find2() {
-        BinarySearch binarySearch = new BinarySearch();
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int target = 10;
-        int result = binarySearch.search(array, target);
-        int resultRecursive = binarySearch.searchRecursive(array, target, 0 , 10);
-        assertEquals(9, result);
-        assertEquals(9, resultRecursive);
+    public static Stream<Arguments> scenarios() {
+        return of(arguments(2, 1), arguments(11, -1), arguments(10, 9), arguments(8, 7), arguments(4, 3));
     }
 
-    @Test
-    public void find3() {
-        BinarySearch binarySearch = new BinarySearch();
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int target = 11;
-        int result = binarySearch.search(array, target);
-        int resultRecursive = binarySearch.searchRecursive(array, target, 0 , 10);
-        assertEquals(-1, result);
-        assertEquals(-1, resultRecursive);
-    }
 }
